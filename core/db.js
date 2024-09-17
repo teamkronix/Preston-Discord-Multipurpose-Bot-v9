28d407a1c9515be986f3c2d56280aca493e17e10
snoww. <155428502+snowded@users.noreply.github.com>
2024-09-17 19:03:35 +0500

    Delete core directory

--- core/db.js
+++
@@ -1,6 +0,0 @@
-const { Database } = require('quickmongo');
-const data = require('../config');
-const db = new Database(data.mongo);
-db.connect().then(() => console.log('[ MONGO DB ] Connected to Mongo Database!'));
-
-module.exports = db;
